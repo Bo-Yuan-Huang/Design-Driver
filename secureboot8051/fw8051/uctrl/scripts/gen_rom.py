9dba1726a58061d93bc35d29f0ddeb61c6779a73
Pramod Subramanyan <psubrama@princeton.edu>
2014-11-26 20:57:33 -0500

    somewhat working ROM generation.

--- secureboot8051/fw8051/uctrl/scripts/gen_rom.py
+++ secureboot8051/fw8051/uctrl/scripts/gen_rom.py
@@ -17,6 +17,49 @@ def read_in(filename):
             data.append(byte)
     return data
 
+def read_hex(filename):
+    data_dict = {}
+    data = []
+    with open(filename, 'rt') as fileobject:
+        for line in fileobject:
+            line = line.strip()
+            if len(line) == 0:
+                continue
+            if line[0] != ':':
+                print filename, line
+            assert line[0] == ':'
+            line = line[1:]
+            length_str = line[:2]
+            addr_str = line[2:6]
+            type_str = line[6:8]
+            data_str = line[8:-2]
+
+            data_length = int(length_str, 16)
+            start_addr = int(addr_str, 16)
+            record_type = int(type_str, 16)
+
+            #print data_str
+            #print 2*data_length, len(data_str)
+            assert 2*data_length == len(data_str)
+            data_bytes = [int(data_str[i:i+2], 16) for i in xrange(0,2*data_length,2)]
+            #print data_bytes
+            assert len(data_bytes) == data_length
+
+            if record_type == 0:
+                for i, addr in enumerate(xrange(start_addr, start_addr+data_length)):
+                    data_dict[addr] = data_bytes[i]
+
+        data = [-1] * (max(data_dict.keys())+1)
+        for addr in data_dict:
+            data_value = data_dict[addr]
+            assert data[addr] == -1
+            data[addr] = data_value
+        for i in xrange(len(data)):
+            if data[i] == -1:
+                data[i] = 0
+
+    return data
+
 def dump_assigns(data):
     N = len(data)
     print '  wire [7:0] buff [%d:0];' % (N-1)
@@ -26,11 +69,16 @@ def dump_assigns(data):
     print '  wire [15:0] addr1 = addr+1;'
     print '  wire [15:0] addr2 = addr+2;'
     print '  wire [15:0] addr3 = addr+3;'
-    print '  wire [7:0] data_o0 = (addr0 < %d) ? buff[addr0] : 8\'h0;' % N
-    print '  wire [7:0] data_o1 = (addr1 < %d) ? buff[addr1] : 8\'h0;' % N
-    print '  wire [7:0] data_o2 = (addr2 < %d) ? buff[addr2] : 8\'h0;' % N
-    print '  wire [7:0] data_o3 = (addr3 < %d) ? buff[addr3] : 8\'h0;' % N
+    print '  wire [7:0] data_o0 = (addr0 < %d) ? buff[addr0] : 8\'hx;' % N
+    print '  wire [7:0] data_o1 = (addr1 < %d) ? buff[addr1] : 8\'hx;' % N
+    print '  wire [7:0] data_o2 = (addr2 < %d) ? buff[addr2] : 8\'hx;' % N
+    print '  wire [7:0] data_o3 = (addr3 < %d) ? buff[addr3] : 8\'hx;' % N
     print '  wire [31:0] data_out = {data_o3, data_o2, data_o1, data_o0};';
 
-data = read_in(sys.argv[1])
-dump_assigns(data)
+#data_hex = read_hex(sys.argv[1])
+#data_in = read_in(sys.argv[2])
+#assert len(data_hex) == len(data_in)
+#assert data_hex == data_in
+#dump_assigns(data_hex)
+#data = read_in(sys.argv[1])
+#dump_assigns(data)
