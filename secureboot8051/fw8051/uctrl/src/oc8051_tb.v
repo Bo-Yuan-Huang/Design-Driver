b5f1c4e587385817aec3187a95a2e29b18477832
Pramod Subramanyan <psubrama@princeton.edu>
2014-12-01 13:27:54 -0500

    some renaming of the signals.

--- secureboot8051/fw8051/uctrl/src/oc8051_tb.v
+++ secureboot8051/fw8051/uctrl/src/oc8051_tb.v
@@ -238,7 +238,7 @@ oc8051_top oc8051_top_1(.wb_rst_i(rst), .wb_clk_i(clk),
 // external data ram
 //
 // oc8051_xram oc8051_xram1 (.clk(clk), .rst(rst), .wr(write_xram), .addr(ext_addr), .data_in(data_out), .data_out(data_out_xram), .ack(ack_xram), .stb(stb_o));
-oc8051_xiommu oc8051_xiommu1 (.clk(clk), .rst(rst), .wr(write_xram), .addr(ext_addr), .data_in(data_out), .data_out(data_out_xram), .ack(ack_xram), .stb(stb_o));
+oc8051_xiommu oc8051_xiommu1 (.clk(clk), .rst(rst), .proc_wr(write_xram), .proc_addr(ext_addr), .proc_data_in(data_out), .proc_data_out(data_out_xram), .proc_ack(ack_xram), .proc_stb(stb_o));
 
 
 defparam oc8051_xiommu1.oc8051_xram_i.DELAY = 2;
--- secureboot8051/fw8051/uctrl/src/oc8051_xiommu.v
+++ secureboot8051/fw8051/uctrl/src/oc8051_xiommu.v
@@ -2,13 +2,23 @@
 `include "oc8051_timescale.v"
 // synopsys translate_on
 
-module oc8051_xiommu (clk, rst, wr, addr, data_in, data_out, ack, stb);
+module oc8051_xiommu (
+    clk, 
+    rst, 
+    proc_wr, 
+    proc_addr, 
+    proc_data_in, 
+    proc_data_out, 
+    proc_ack, 
+    proc_stb
+);
 
-input clk, wr, stb, rst;
-input [7:0] data_in;
-input [15:0] addr;
-output [7:0] data_out;
-output ack;
+input clk, rst; 
+input proc_wr, proc_stb;
+input [7:0] proc_data_in;
+input [15:0] proc_addr;
+output [7:0] proc_data_out;
+output proc_ack;
 
 wire write_xram, write_aes;
 wire ack_xram, ack_aes;
@@ -16,22 +26,22 @@ wire stb_xram, stb_aes;
 wire aes_addr_range;
 
 // STROBE.
-assign stb_aes = stb && aes_addr_range;
-assign stb_xram = stb && !aes_addr_range;
+assign stb_aes = proc_stb && aes_addr_range;
+assign stb_xram = proc_stb && !aes_addr_range;
 
 // WRITE.
-assign write_xram = stb_xram && wr;
-assign write_aes = stb_aes && wr;
+assign write_xram = stb_xram && proc_wr;
+assign write_aes = stb_aes && proc_wr;
 
 // ACK OUTPUT.
-wire ack = ack_xram || ack_aes;
+wire proc_ack = ack_xram || ack_aes;
 
 // DATA OUT.
-wire [7:0] data_out;
+wire [7:0] proc_data_out;
 wire [7:0] data_out_xram;
 wire [7:0] data_out_aes;
 
-assign data_out = stb_aes ? data_out_aes : data_out_xram;
+assign proc_data_out = stb_aes ? data_out_aes : data_out_xram;
 
 // AES <=> XRAM signals
 wire [15:0] aes_xram_addr;
@@ -45,8 +55,8 @@ aes_top aes_top_i (
     .clk           ( clk               ),
     .rst           ( rst               ),
     .wr            ( write_aes         ),
-    .addr          ( addr              ),
-    .data_in       ( data_in           ),
+    .addr          ( proc_addr         ),
+    .data_in       ( proc_data_in      ),
     .data_out      ( data_out_aes      ),
     .ack           ( ack_aes           ),
     .stb           ( stb_aes           ),
@@ -71,8 +81,8 @@ oc8051_memarbiter oc8051_memarbiter_i (
     .stb_A      ( stb_xram          ),
     .ack_A      ( ack_xram          ),
     .wr_A       ( write_xram        ),
-    .addr_A     ( addr              ),
-    .data_in_A  ( data_in           ),
+    .addr_A     ( proc_addr              ),
+    .data_in_A  ( proc_data_in      ),
     .data_out_A ( data_out_xram     ),
 
     .stb_B      ( aes_xram_stb      ),
