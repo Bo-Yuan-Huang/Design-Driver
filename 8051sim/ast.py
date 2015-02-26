116bbdbda2de71446434eabb3a7c12b5a6f31bc9
Pramod Subramanyan <psubrama@princeton.edu>
2015-02-26 16:08:35 -0500

    some improvements to the synthesizer.

--- 8051sim/ast.py
+++ 8051sim/ast.py
@@ -51,33 +51,43 @@ from z3helper import *
 class Node(object):
     """Base class for nodes in the AST."""
     NODE_TYPE_MIN   = 0
-    BOOL            = 0
-    BITVEC          = 1
-    CHOICE          = 2
-    PLUS            = 3
-    NODE_TYPE_MAX   = 3
+    BOOLVAR         = 0
+    BITVECVAR       = 1
+    BITVECVAL       = 2
+    CHOICE          = 3
+    Z3OP            = 4
+    NODE_TYPE_MAX   = 4
 
     def __init__(self, nodetype):
         assert nodetype >= Node.NODE_TYPE_MIN
         assert nodetype <= Node.NODE_TYPE_MAX
         self.nodetype = nodetype
+        self.z3obj = None
+        self.z3prefix = ''
+        self.name = 'UnnamedObject'
 
-    def declare(self, prefix='', ctx=None):
-        err_msg = 'Declare not implemented in %s' % self.__class__.__name__
+    def _getName(self, prefix):
+        prefstr = prefix + '__' if len(prefix) > 0 else ''
+        return prefstr + self.name
+
+    def _toZ3(self, prefix):
+        err_msg = '_toZ3 not implemented in %s' % self.__class__.__name__
         raise NotImplementedError, err_msg
+
+    def toZ3(self, prefix=''):
+        if self.z3obj is None or self.z3prefix != prefix:
+            self.z3obj = self._toZ3(prefix)
+            self.z3prefix = prefix
+        return self.z3obj
 
 class BoolVar(Node):
     """Boolean variables."""
     def __init__(self, name):
-        Node.__init__(self, Node.BOOL)
+        Node.__init__(self, Node.BOOLVAR)
         self.name = name
 
-    def declare(self, prefix='', ctx=None):
-        prefstr = prefix + '__' if len(prefix) > 0 else ''
-        declname = prefstr + self.name
-        if ctx == None:
-            ctx = z3.main_ctx()
-        return z3.Bool(declname, ctx)
+    def _toZ3(self, prefix):
+        return z3.Bool(self._getName(prefix))
 
     def __str__(self):
         return 'bool(%s)' % self.name
@@ -86,25 +96,106 @@ class BitVecVar(Node):
     """Bitvector variables."""
 
     def __init__(self, name, width):
-        Node.__init__(self, Node.BITVEC)
+        Node.__init__(self, Node.BITVECVAR)
         self.name = name
         self.width = width
 
-    def declare(self, prefix='', ctx=None):
-        prefstr = prefix + '__' if len(prefix) > 0 else ''
-        declname = prefstr + self.name
-        if ctx == None:
-            ctx = z3.main_ctx()
-        return z3.BitVec(declname, self.width, ctx)
+    def _toZ3(self, prefix):
+        return z3.BitVec(self._getName(prefix), self.width)
 
     def __str__(self):
         return 'bitvec(%s, %d)' % (self.name, self.width)
 
+class BitVecVal(Node):
+    """BitVector Constants."""
+    def __init__(self, value, width):
+        Node.__init__(self, Node.BITVECVAL)
+        self.value = value
+        self.width = width
+
+    def _toZ3(self, prefix):
+        return z3.BitVecVal(self.value, self.width)
+
+    def __str__(self):
+        return 'bitvec(%d, %d)' % (self.value, self.width)
+
+class Choice(Node):
+    """A choice between a set of options."""
+    def __init__(self, name, choiceVar, choices):
+        Node.__init__(self, Node.CHOICE)
+        self.name = name
+        self.choiceVar = choiceVar
+        self.choices = choices
+
+    def _toZ3(self, prefix):
+        assert len(self.choices) > 1
+
+        self.choiceBools = []
+        for i in xrange(len(self.choices)-1):
+            boolName = '%s__choice__%d' % (self.name, i)
+            self.choiceBools.append(Bool(boolName))
+
+        def createIf(i):
+            if i < len(self.choiceBools):
+                cvi = self.choiceBools[i]
+                return If(cvi, self.choices[i].toZ3(prefix), createIf(i+1))
+            else:
+                assert i == (len(self.choices) - 1)
+                return self.choices[i].toZ3()
+        return createIf(0)
+
+class Z3Op(Node):
+    """Binary operators from Z3."""
+    def __init__(self, opname, op, operands):
+        Node.__init__(self, Node.Z3OP)
+        self.op = op
+        self.opname = opname
+        self.operands = operands
+
+    def _toZ3(self, prefix):
+        return self.op(*[x.toZ3(prefix) for x in self.operands])
+
+    def __str__(self):
+        return '%s(%s)' % (self.opname, ', '.join(str(x) for x in self.operands))
+
+def Add(*operands):
+    return Z3Op('add', BVAdd, operands)
+
+def Sub(*operands):
+    return Z3Op('sub', BVSub, operands)
+
+def Neg(operand):
+    return Z3Op('neg', BVNeg, [operand])
+
+def Distinct(op1, op2):
+    return Z3Op('distinct', z3.Distinct, [op1, op2])
+
 def test_AST():
     v1 = BoolVar('b1')
     v2 = BitVecVar('b2', 16)
+    v3 = BitVecVal(2, 16)
     assert str(v1) == 'bool(b1)'
     assert str(v2) == 'bitvec(b2, 16)'
+    assert str(v3) == 'bitvec(2, 16)'
+    v4 = Add(v2, v3)
+    v5 = Sub(v2, v3)
+    print v1.toZ3(), v2.toZ3(), v3.toZ3(), v4.toZ3(), v5.toZ3()
+
+    S = z3.Solver()
+    S.add(v4.toZ3() == v5.toZ3())
+    assert S.check() == z3.unsat
+
+    S = z3.Solver()
+    v6 = Neg(v2)
+    S.add(v2.toZ3() == v6.toZ3())
+    assert S.check() == z3.sat
+    m = S.model()
+    print m
+
+    c1 = Choice('choice', None, [v4, v5, v6])
+    print c1.toZ3()
+
+
     print 'Var assertions passed.'
 
 
--- 8051sim/z3helper.py
+++ 8051sim/z3helper.py
@@ -1,5 +1,25 @@
 from z3 import *
 
+def _mkBVBinOp(op, *xs):
+    assert len(xs) >= 2
+    si = BitVecRef(op(xs[0].ctx_ref(), xs[0].as_ast(), xs[1].as_ast()), xs[0].ctx)
+    for i in xrange(2, len(xs)):
+        xi = xs[i]
+        si = BitVecRef(op(si.ctx_ref(), si.as_ast(), xi.as_ast()), si.ctx)
+    return si
+
+def _mkBVUnOp(op, x):
+    return BitVecRef(op(x.ctx_ref(), x.as_ast()), x.ctx)
+
+def BVAdd(*xs):
+    return _mkBVBinOp(Z3_mk_bvadd, *xs)
+
+def BVSub(*xs):
+    return _mkBVBinOp(Z3_mk_bvsub, *xs)
+
+def BVNeg(x):
+    return _mkBVUnOp(Z3_mk_bvneg, x)
+
 def BVRedAnd(a):
     return BitVecRef(Z3_mk_bvredand(a.ctx_ref(), a.as_ast()), a.ctx)
 
