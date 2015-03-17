0d065b082cb37941fc1a9334eeee22290962d78f
Pramod S <pramod.subramanyan@gmail.com>
2015-03-17 00:36:23 -0400

    modernized sim51test

--- 8051sim/sim51test.py
+++ 8051sim/sim51test.py
@@ -1,15 +1,33 @@
-from sim51 import evalPC, dumpState
+from uc8051syn import eval8051
 
-# 107E  101B7 [ 1:20 11:fd fd: 1 150:10] --> 1082
-pc = 0x107E
-opcode = 0x101B7 
-regs = [0] * 384
-regs[0x1]   = 0x20
-regs[0x11]  = 0xFD
-regs[0xFD]  = 0x1
-regs[0x150] = 0x10
+sim_inputs = {
+    'ACC':0, 
+    'PCON':0, 
+    'B':0, 
+    'TMOD':0, 
+    'DPL':0, 
+    'DPH':0, 
+    'TL1':0, 
+    'TL0':0, 
+    'IE':0, 
+    'TH1':0, 
+    'TH0':0, 
+    'PC':0xfffe, 
+    'P2':0, 
+    'P3':0, 
+    'P0':0, 
+    'P1':0, 
+    'IRAM':[[0x10,1], [0,0], [1,0x10], 1], 
+    'IP':0, 
+    'TCON':0, 
+    'SCON':0, 
+    'SP':0, 
+    'SBUF':0, 
+    'opcode':0x100bc, 
+    'PSW':4
+}
 
-print '%x' % evalPC(pc, opcode, regs)
-with open('dbgstate', 'wt') as fileobject:
-    dumpState(fileobject, pc, opcode, regs)
+sim_outputs = {}
 
+eval8051(sim_inputs, sim_outputs)
+print sim_outputs
