2242fefb00bfde69c342defc1c75d008024e4227
Pramod Subramanyan <pramod.subramanyan@gmail.com>
2015-03-24 16:25:25 -0400

    reworked the ctx8051 object to be cleaner.

--- 8051sim/uc8051ast.py
+++ 8051sim/uc8051ast.py
@@ -1,148 +1,126 @@
 from ast import *
 
+def create8051Inputs(syn):
+    syn.addInput(BitVecVar('PC', 16))
+    syn.addInput(BitVecVar('opcode', 24))
+    # IRAM
+    syn.addInput(MemVar('IRAM', 8, 8))
+    # main SFRs
+    syn.addInput(BitVecVar('ACC', 8))
+    syn.addInput(BitVecVar('B', 8))
+    syn.addInput(BitVecVar('PSW', 8))
+    syn.addInput(BitVecVar('SP', 8))
+    syn.addInput(BitVecVar('DPL', 8))
+    syn.addInput(BitVecVar('DPH', 8))
+    # ports
+    syn.addInput(BitVecVar('P0', 8))
+    syn.addInput(BitVecVar('P1', 8))
+    syn.addInput(BitVecVar('P2', 8))
+    syn.addInput(BitVecVar('P3', 8))
+    # misc SFRs.
+    syn.addInput(BitVecVar('PCON', 8))
+    syn.addInput(BitVecVar('TCON', 8))
+    syn.addInput(BitVecVar('TMOD', 8))
+    syn.addInput(BitVecVar('TL0', 8))
+    syn.addInput(BitVecVar('TH0', 8))
+    syn.addInput(BitVecVar('TL1', 8))
+    syn.addInput(BitVecVar('TH1', 8))
+    syn.addInput(BitVecVar('SCON', 8))
+    syn.addInput(BitVecVar('SBUF', 8))
+    syn.addInput(BitVecVar('IE', 8))
+    syn.addInput(BitVecVar('IP', 8))
+
 class Ctx8051(object):
-    def __init__(self, syn):
-        # Create the inputs.
-        # These two are for the synthesis. 
-        # Need to think about replacing this with a memory abstraction.
-        syn.addInput(BitVecVar('PC', 16))
-        syn.addInput(BitVecVar('opcode', 24))
-        # IRAM
-        syn.addInput(MemVar('IRAM', 8, 8))
-        # main SFRs
-        syn.addInput(BitVecVar('ACC', 8))
-        syn.addInput(BitVecVar('B', 8))
-        syn.addInput(BitVecVar('PSW', 8))
-        syn.addInput(BitVecVar('SP', 8))
-        syn.addInput(BitVecVar('DPL', 8))
-        syn.addInput(BitVecVar('DPH', 8))
-        # ports
-        syn.addInput(BitVecVar('P0', 8))
-        syn.addInput(BitVecVar('P1', 8))
-        syn.addInput(BitVecVar('P2', 8))
-        syn.addInput(BitVecVar('P3', 8))
-        # misc SFRs.
-        syn.addInput(BitVecVar('PCON', 8))
-        syn.addInput(BitVecVar('TCON', 8))
-        syn.addInput(BitVecVar('TMOD', 8))
-        syn.addInput(BitVecVar('TL0', 8))
-        syn.addInput(BitVecVar('TH0', 8))
-        syn.addInput(BitVecVar('TL1', 8))
-        syn.addInput(BitVecVar('TH1', 8))
-        syn.addInput(BitVecVar('SCON', 8))
-        syn.addInput(BitVecVar('SBUF', 8))
-        syn.addInput(BitVecVar('IE', 8))
-        syn.addInput(BitVecVar('IP', 8))
+    def __init__( self, PC, opcode, IRAM, P0, SP, DPL, DPH, PCON, TCON, TMOD, 
+        TL0, TH0, TL1, TH1, P1, SCON, SBUF, P2, IE, P3, IP, PSW, ACC, B):
 
-        # Create aliases for them.
-        self.syn = syn
-        self.PC = syn.inp('PC')
-        self.opcode = syn.inp('opcode')
-        self.IRAM = syn.inp('IRAM')
-        self.P0 = syn.inp('P0')
-        self.SP = syn.inp('SP')
-        self.DPL = syn.inp('DPL')
-        self.DPH = syn.inp('DPH')
-        self.PCON = syn.inp('PCON')
-        self.TCON = syn.inp('TCON')
-        self.TMOD = syn.inp('TMOD')
-        self.TL0 = syn.inp('TL0')
-        self.TH0 = syn.inp('TH0')
-        self.TL1 = syn.inp('TL1')
-        self.TH1 = syn.inp('TH1')
-        self.P1 = syn.inp('P1')
-        self.SCON = syn.inp('SCON')
-        self.SBUF = syn.inp('SBUF')
-        self.P2 = syn.inp('P2')
-        self.IE = syn.inp('IE')
-        self.P3 = syn.inp('P3')
-        self.IP = syn.inp('IP')
-        self.PSW = syn.inp('PSW')
-        self.ACC = syn.inp('ACC')
-        self.B = syn.inp('B')
+        self.PC = PC
+        self.opcode = opcode
+        self.IRAM = IRAM
+        self.P0 = P0
+        self.SP = SP
+        self.DPL = DPL
+        self.DPH = DPH
+        self.PCON = PCON
+        self.TCON = TCON
+        self.TMOD = TMOD
+        self.TL0 = TL0
+        self.TH0 = TH0
+        self.TL1 = TL1
+        self.TH1 = TH1
+        self.P1 = P1
+        self.SCON = SCON
+        self.SBUF = SBUF
+        self.P2 = P2
+        self.IE = IE
+        self.P3 = P3
+        self.IP = IP
+        self.PSW = PSW
+        self.ACC = ACC
+        self.B = B
 
-class DirectIRAMRead(Node):
-    "Perform a direct read from the 8051 IRAM."
-    DIR_IRAM_READ = Node.NODE_TYPE_MAX+1
+        self._CY = Extract(7, 7, self.PSW)
+        self._Rbank = Extract(4, 3, self.PSW)
+        self._RxAddr = [Concat(BitVecVal(0, 3), self._Rbank, BitVecVal(i, 3)) for i in xrange(8)]
+        self._Rx = [ReadMem(self.IRAM, RxAddr_i) for RxAddr_i in self._RxAddr]
 
-    def __init__(self, ctx, addr):
-        if addr.width != 8:
-            raise ValueError, "Address width must be 8."
-        assert ctx.IRAM.dwidth == 8
+    def CY(self):
+        return self._CY
 
-        Node.__init__(self, self.DIR_IRAM_READ)
-        self.addr = addr
-        self.width = ctx.IRAM.dwidth
-        self.ctx = ctx
-         
-    def _toZ3sHelper(self, prefix, rfun):
-        mz3 = rfun(self.ctx.IRAM, prefix)
-        az3 = rfun(self.addr, prefix)
+    def RxAddr(self, i):
+        return self._RxAddr[i]
 
-        msb0 = z3.Extract(7, 7, az3) == z3.BitVecVal(0, 1)
-        return z3.If(msb0, mz3[az3],
-            z3.If(az3 == z3.BitVecVal(0x80, 8), rfun(self.ctx.P0, prefix),
-            z3.If(az3 == z3.BitVecVal(0x81, 8), rfun(self.ctx.SP, prefix),
-            z3.If(az3 == z3.BitVecVal(0x82, 8), rfun(self.ctx.DPL, prefix),
-            z3.If(az3 == z3.BitVecVal(0x83, 8), rfun(self.ctx.DPH, prefix),
-            z3.If(az3 == z3.BitVecVal(0x87, 8), rfun(self.ctx.PCON, prefix),
-            z3.If(az3 == z3.BitVecVal(0x88, 8), rfun(self.ctx.TCON, prefix),
-            z3.If(az3 == z3.BitVecVal(0x89, 8), rfun(self.ctx.TMOD, prefix),
-            z3.If(az3 == z3.BitVecVal(0x8A, 8), rfun(self.ctx.TL0, prefix),
-            z3.If(az3 == z3.BitVecVal(0x8C, 8), rfun(self.ctx.TH0, prefix),
-            z3.If(az3 == z3.BitVecVal(0x8B, 8), rfun(self.ctx.TL1, prefix),
-            z3.If(az3 == z3.BitVecVal(0x8D, 8), rfun(self.ctx.TH1, prefix),
-            z3.If(az3 == z3.BitVecVal(0x90, 8), rfun(self.ctx.P1, prefix),
-            z3.If(az3 == z3.BitVecVal(0x98, 8), rfun(self.ctx.SCON, prefix),
-            z3.If(az3 == z3.BitVecVal(0x99, 8), rfun(self.ctx.SBUF, prefix),
-            z3.If(az3 == z3.BitVecVal(0xA0, 8), rfun(self.ctx.P2, prefix),
-            z3.If(az3 == z3.BitVecVal(0xA8, 8), rfun(self.ctx.IE, prefix),
-            z3.If(az3 == z3.BitVecVal(0xB0, 8), rfun(self.ctx.P3, prefix),
-            z3.If(az3 == z3.BitVecVal(0xB8, 8), rfun(self.ctx.IP, prefix),
-            z3.If(az3 == z3.BitVecVal(0xD0, 8), rfun(self.ctx.PSW, prefix),
-            z3.If(az3 == z3.BitVecVal(0xE0, 8), rfun(self.ctx.ACC, prefix),
-            z3.If(az3 == z3.BitVecVal(0xF0, 8), rfun(self.ctx.B, prefix),
-            z3.BitVecVal(0, 8)))))))))))))))))))))))
+    def RxAddrs(self):
+        return self._RxAddr
 
-    def _toZ3(self, prefix):
-        rfun = lambda n, prefix : n.toZ3(prefix)
-        return self._toZ3sHelper(prefix, rfun)
+    def Rx(self, i):
+        return self._Rx[i]
 
-    def _toZ3Constraints(self, prefix, m):
-        rfun = lambda n, prefix : n.toZ3Constraints(prefix, m)
-        return self._toZ3sHelper(prefix, rfun)
+    def Rxs(self):
+        return self._Rx
 
-    def __str__(self):
-        return '(read-direct %s %s)' % (str(self.ctx.IRAM), str(self.addr))
+    def readDirect(self, addr):
+        msb0 = Equal(Extract(7, 7, addr), BitVecVal(0, 1))
+        return If(msb0, ReadMem(self.IRAM, addr),
+            If(Equal(addr, BitVecVal(0x80, 8)), self.P0,
+            If(Equal(addr, BitVecVal(0x81, 8)), self.SP,
+            If(Equal(addr, BitVecVal(0x82, 8)), self.DPL,
+            If(Equal(addr, BitVecVal(0x83, 8)), self.DPH,
+            If(Equal(addr, BitVecVal(0x87, 8)), self.PCON,
+            If(Equal(addr, BitVecVal(0x88, 8)), self.TCON,
+            If(Equal(addr, BitVecVal(0x89, 8)), self.TMOD,
+            If(Equal(addr, BitVecVal(0x8A, 8)), self.TL0,
+            If(Equal(addr, BitVecVal(0x8C, 8)), self.TH0,
+            If(Equal(addr, BitVecVal(0x8B, 8)), self.TL1,
+            If(Equal(addr, BitVecVal(0x8D, 8)), self.TH1,
+            If(Equal(addr, BitVecVal(0x90, 8)), self.P1,
+            If(Equal(addr, BitVecVal(0x98, 8)), self.SCON,
+            If(Equal(addr, BitVecVal(0x99, 8)), self.SBUF,
+            If(Equal(addr, BitVecVal(0xA0, 8)), self.P2,
+            If(Equal(addr, BitVecVal(0xA8, 8)), self.IE,
+            If(Equal(addr, BitVecVal(0xB0, 8)), self.P3,
+            If(Equal(addr, BitVecVal(0xB8, 8)), self.IP,
+            If(Equal(addr, BitVecVal(0xD0, 8)), self.PSW,
+            If(Equal(addr, BitVecVal(0xE0, 8)), self.ACC,
+            If(Equal(addr, BitVecVal(0xF0, 8)), self.B,
+            BitVecVal(0, 8)))))))))))))))))))))))
 
-    def synthesize(self, m):
-        return DirectIRAMRead(self.ctx, self.addr.synthesize(m))
+    def readBit(self, bitaddr):
+        msb1 = Equal(Extract(7, 7, bitaddr), BitVecVal(1, 1))
+        byteaddr = If(msb1, 
+            Concat(Extract(7, 3, bitaddr), BitVecVal(0, 3)), 
+            Add(ZeroExt(Extract(7, 3, bitaddr), 3), BitVecVal(32, 8)))
+        bitindex = Extract(2, 0, bitaddr)
+        byte = self.readDirect(byteaddr)
+        bit = ExtractBit(byte, bitindex)
+        return bit
 
-    def childObjects(self):
-        yield self.ctx.IRAM
-        yield self.addr
-        yield self.ctx.P0
-        yield self.ctx.SP
-        yield self.ctx.DPL
-        yield self.ctx.DPH
-        yield self.ctx.PCON
-        yield self.ctx.TCON
-        yield self.ctx.TMOD
-        yield self.ctx.TL0
-        yield self.ctx.TH0
-        yield self.ctx.TL1
-        yield self.ctx.TH1
-        yield self.ctx.P1
-        yield self.ctx.SCON
-        yield self.ctx.SBUF
-        yield self.ctx.P2
-        yield self.ctx.IE
-        yield self.ctx.P3
-        yield self.ctx.IP
-        yield self.ctx.PSW
-        yield self.ctx.ACC
-        yield self.ctx.B
-
+def Ctx8051FromSyn(syn):
+    return Ctx8051( syn.inp('PC'), syn.inp('opcode'), syn.inp('IRAM'), syn.inp('P0'), syn.inp('SP'), 
+        syn.inp('DPL'), syn.inp('DPH'), syn.inp('PCON'), syn.inp('TCON'), syn.inp('TMOD'), 
+        syn.inp('TL0'), syn.inp('TH0'), syn.inp('TL1'), syn.inp('TH1'), syn.inp('P1'), syn.inp('SCON'), 
+        syn.inp('SBUF'), syn.inp('P2'), syn.inp('IE'), syn.inp('P3'), syn.inp('IP'), syn.inp('PSW'), 
+        syn.inp('ACC'), syn.inp('B') )
 
 class DirectIRAMWrite(Node):
     """Perform on a direct write from the 8051 IRAM. This might 
--- 8051sim/uc8051syn.py
+++ 8051sim/uc8051syn.py
@@ -2,33 +2,19 @@ from synthesizer import Synthesizer
 from ast import *
 from sim51 import evalState
 import sys
-from uc8051ast import DirectIRAMRead, DirectIRAMWrite, Ctx8051
+from uc8051ast import Ctx8051, Ctx8051FromSyn, create8051Inputs
 from uc8051sim import eval8051
 
 def synthesize():
     syn = Synthesizer()
-    ctx = Ctx8051(syn)
+    create8051Inputs(syn)
+    ctx = Ctx8051FromSyn(syn)
 
-    ctx.SP_plus1 = Add(ctx.SP, BitVecVal(1, 8))
-    ctx.SP_minus1 = Sub(ctx.SP, BitVecVal(1, 8))
     mem_SP = ReadMem(ctx.IRAM, ctx.SP)
-    mem_SP_plus1 = ReadMem(ctx.IRAM, ctx.SP_plus1)
-    mem_SP_minus1 = ReadMem(ctx.IRAM, ctx.SP_minus1)
-
+    mem_SP_plus1 = ReadMem(ctx.IRAM, Add(ctx.SP, BitVecVal(1, 8)))
+    mem_SP_minus1 = ReadMem(ctx.IRAM, Sub(ctx.SP, BitVecVal(1, 8)))
     DPTR = Concat(ctx.DPH, ctx.DPL)
 
-    CY = ChooseConsecBits('PSW_CY_bit', 1, ctx.PSW)
-
-    # registers.
-    Rbank = ChooseConsecBits('Rbank_bits', 2, ctx.PSW)
-    Rxaddr = []
-    Rx = []
-    for i in xrange(8):
-        Rxaddr.append(Concat(BitVecVal(0, 3), Rbank, BitVecVal(i, 3)))
-        Rx.append(ReadMem(ctx.IRAM, Rxaddr[-1]))
-    assert len(Rx) == 8
-    assert len(Rxaddr) == 8
-    
     op0 = Extract(7, 0, ctx.opcode)
     op1 = Extract(15, 8, ctx.opcode)
     op2 = Extract(23, 16, ctx.opcode)
@@ -50,19 +36,15 @@ def synthesize():
     PC_ljmp = Choice('LJMP_order', op0, [Concat(op1, op2), Concat(op2, op1)])
     PC_sjmp = Choice('SJMP_relpc', op0, [PC_rel1, PC_rel2])
     
-    jb_bit_addr = Choice('bit_addr_choice', op0, [op1, op2])
-    jb_msb_set = Equal(Extract(7, 7, jb_bit_addr), BitVecVal(1, 1))
-    jb_byte_addr = If(jb_msb_set, Concat(Extract(7, 3, jb_bit_addr), BitVecVal(0, 3)), Add(ZeroExt(Extract(7, 3, jb_bit_addr), 3), BitVecVal(32, 8)))
-    jb_bit_num = Extract(2, 0, jb_bit_addr)
-    jb_byte = DirectIRAMRead(ctx, jb_byte_addr)
-    jb_bit = ExtractBit(jb_byte, jb_bit_num)
+    
+    jb_bit = ctx.readBit(Choice('bit_addr_choice', op0, [op1, op2]))
     PC_jb_taken = Choice('PC_jb_rel', op0, [PC_rel1, PC_rel2])
     PC_jb_seq = Choice('PC_jb_seq', op0, [PC_plus2, PC_plus3])
     PC_jb = If(Equal(jb_bit, Choice('jb_polarity', op0, [BitVecVal(1,1), BitVecVal(0,1)])), PC_jb_taken, PC_jb_seq)
 
     PC_jc_taken = Choice('PC_jc_rel', op0, [PC_rel1, PC_rel2])
     PC_jc_seq = Choice('PC_jc_seq', op0, [PC_plus2, PC_plus3])
-    PC_jc = If(Equal(CY, Choice('jc_polarity', op0, [BitVecVal(1,1), BitVecVal(0,1)])), PC_jc_taken, PC_jc_seq)
+    PC_jc = If(Equal(ctx.CY(), Choice('jc_polarity', op0, [BitVecVal(1,1), BitVecVal(0,1)])), PC_jc_taken, PC_jc_seq)
 
     PC_jz_taken = Choice('PC_jz_rel', op0, [PC_rel1, PC_rel2])
     PC_jz_seq = Choice('PC_jz_seq', op0, [PC_plus2, PC_plus3])
@@ -71,14 +53,14 @@ def synthesize():
     PC_jz = If(Choice('jz_polarity', op0, [ACC_zero, ACC_not_zero]), PC_jz_taken, PC_jz_seq)
     PC_jmp = Add(DPTR, SignExt(ctx.ACC, 8))
 
-    cjne_src1 = Choice('cjne_src1', op0, [ctx.ACC, ReadMem(ctx.IRAM, Rx[0]), ReadMem(ctx.IRAM, Rx[1])] + Rx)
-    cjne_src2 = Choice('cjne_src2', op0, [op1, op2, DirectIRAMRead(ctx, Choice('cjne_iram_addr', op0, [op1, op2]))])
+    cjne_src1 = Choice('cjne_src1', op0, [ctx.ACC, ReadMem(ctx.IRAM, ctx.Rx(0)), ReadMem(ctx.IRAM, ctx.Rx(1))] + ctx.Rxs())
+    cjne_src2 = Choice('cjne_src2', op0, [op1, op2, ctx.readDirect(Choice('cjne_iram_addr', op0, [op1, op2]))])
     cjne_taken = Not(Equal(cjne_src1, cjne_src2))
     PC_cjne_taken = Choice('PC_cjne_taken', op0, [PC_rel1, PC_rel2])
     PC_cjne_seq = Choice('PC_cjne_seq', op0, [PC_plus2, PC_plus3])
     PC_cjne = If(cjne_taken, PC_cjne_taken, PC_cjne_seq)
 
-    djnz_src = Choice('djnz_src', op0, [DirectIRAMRead(ctx, Choice('djnz_iram_src', op0, [op1, op2]))] + Rx)
+    djnz_src = Choice('djnz_src', op0, [ctx.readDirect(Choice('djnz_iram_src', op0, [op1, op2]))] + ctx.Rxs())
     djnz_taken = Not(Equal(djnz_src, BitVecVal(1, 8)))
     PC_djnz_taken = Choice('PC_djnz_taken', op0, [PC_rel1, PC_rel2])
     PC_djnz_seq = Choice('PC_djnz_seq', op0, [PC_plus2, PC_plus3])
@@ -94,12 +76,12 @@ def synthesize():
     # INC_SRC1_INDIR_ADDR = Choice('INC_SRC1_INDIR', op0, [Rx[0], Rx[1]])
     # INC_SRC1_INDIR = ReadMem(ctx.IRAM, INC_SRC1_INDIR_ADDR)
 
-    INC_SRC1_ADDR = Choice('INC_SRC1_ADDR', op0, [op1, op2] + Rxaddr)
+    INC_SRC1_ADDR = Choice('INC_SRC1_ADDR', op0, [op1, op2] + ctx.RxAddrs())
     INC_SRC1_ADDR_msb0 = Equal(Extract(7, 7, INC_SRC1_ADDR), BitVecVal(0, 1))
-    INC_SRC1_INDIR_ADDR = Choice('INC_SRC1_INDIR_ADDR', op0, [Rx[0], Rx[1]])
+    INC_SRC1_INDIR_ADDR = Choice('INC_SRC1_INDIR_ADDR', op0, [ctx.Rx(0), ctx.Rx(1)])
     INC_SRC1_INDIR = ReadMem(ctx.IRAM, INC_SRC1_INDIR_ADDR)
 
-    INC_SRC1_DIR = DirectIRAMRead(ctx, INC_SRC1_ADDR)
+    INC_SRC1_DIR = ctx.readDirect(INC_SRC1_ADDR)
     INC_SRC1 = Choice('INC_SRC1', op0, [INC_SRC1_DIR, INC_SRC1_INDIR, ctx.ACC])
 
     INC_RES = Add(INC_SRC1, BitVecVal(1, 8))
@@ -117,10 +99,13 @@ def synthesize():
     # syn.unsat_core = True
     # syn.logfile = sys.stdout # open('debug.log', 'wt')
     # for opcode in [0x76]:
-    for opc in [0x04, 0x05, 0x6, 0x7] + range(0x08, 0x10): # range(0x100):
+    # for opc in [0x04, 0x05, 0x6, 0x7] + range(0x08, 0x10): # range(0x100):
+    for opc in xrange(0x100):
         cnst = Equal(op0, BitVecVal(opc, 8))
-        [pc, acc, iram] = syn.synthesize(['PC', 'ACC', 'IRAM'], [cnst], eval8051)
-        print '%02x\n%s\n%s\n%s\n' % (opc, pc, acc, iram)
+        [pc] = syn.synthesize(['PC'], [cnst], eval8051)
+        print '%02x\n%s\n' % (opc, str(pc))
+        # [pc, acc, iram] = syn.synthesize(['PC', 'ACC', 'IRAM'], [cnst], eval8051)
+        # print '%02x\n%s\n%s\n%s\n' % (opc, pc, acc, iram)
 
 
 if __name__ == '__main__':
