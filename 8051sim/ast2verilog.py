56973dce9f35169afe0b8e8bfde6efb573423b7a
Pramod S <pramod.subramanyan@gmail.com>
2015-04-09 23:37:52 -0400

    some more progress.

--- 8051sim/ast2verilog.py
+++ 8051sim/ast2verilog.py
@@ -32,6 +32,58 @@ def readAllASTs(d):
 
     return asts
 
+class VerilogContext(object):
+    def __init__(self):
+        self.inputs     = []
+        self.mems       = []
+        self.wires      = []
+        self.outputs    = []
+        self.memwrites  = []
+        self.statements = []
+        self.objects    = {}
+        self.nameCtr    = 0
+
+
+    def _getName(self):
+        self.nameCtr += 1
+        return 'n%05d' % self.nameCtr
+
+    def addInput(self, node, width):
+        if node not in self.objects:
+            self.inputs.append((name, width))
+            self.objects[node] = name
+    
+    def addMem(self, node):
+        sz = 1 << node.awidth
+        self.mems.append(node)
+        self.objects[node] = node.name
+
+    def writeMem(self, mem, addr, data):
+        assert mem in self.objects
+        self.memwrites.append((mem, addr, data))
+
+    def addWire(self, node, name, width):
+        assert node not in self.objects
+        self.wires.append((name, width))
+        self.objects[node] = name
+
+    def getExpr(self, node):
+        if node not in self.objects:
+            try:
+                w = node.width
+                width = (w-1, 0)
+            except AttributeError:
+                width = None
+
+            name = self._getName()
+            self.addWire(node, name, width)
+            obj = node2verilog(node, self)
+            self.objects[node] = obj
+        return self.objects[node]
+
+    def dump(self, f):
+        pass
+
 def node2verilog(node, ctx):
     if node.nodetype == ast.Node.BOOLVAR:
         return boolvar2verilog(node, ctx)
@@ -64,12 +116,12 @@ def node2verilog(node, ctx):
 
 def boolvar2verilog(node, ctx):
     assert node.nodetype == ast.Node.BOOLVAR
-    ctx.addInput(node, None, node.name)
+    ctx.addInput(node, None)
     return node.name
 
 def bitvecvar2verilog(node, ctx):
     assert node.nodetype == ast.Node.BITVECVAR
-    ctx.addInput(node, (node.width-1, 0), node.name)
+    ctx.addInput(node, (node.width-1, 0))
     return node.name
 
 def boolvar2verilog(node, ctx):
@@ -82,18 +134,19 @@ def bitvecval2verilog(node, ctx):
 
 def memvar2verilog(node, ctx):
     assert node.nodetype == ast.Node.MEMVAR
-    sz = 1 << node.awidth
-    ctx.addMem(name, sz, node.dwidth)
+    ctx.addMem(node)
     return node
 
 def readmem2verilog(node, ctx):
     assert node.nodetype == ast.Node.READMEM
-    mem = ctx.getMem(node.mem)
+    mem = ctx.getExpr(node.mem)
     addr = ctx.getExpr(node.addr)
     return '%s[%s]' % (mem, addr)
 
 def writemem2verilog(node, ctx):
     assert node.nodetype == ast.Node.WRITEMEM
+    assert node.mem.nodetype == ast.Node.MEMVAR
+
     addr = ctx.getExpr(node.addr)
     data = ctx.getExpr(node.data)
     ctx.writeMem(node.mem, addr, data)
@@ -128,27 +181,31 @@ def macro2verilog(node, ctx):
 def z3op2verilog(node, ctx):
     assert node.nodetype == ast.Node.Z3OP
     ops = [ctx.getExpr(op) for op in node.operands]
-    params = [ctx.getExpr(p) for p in node.params]
+    if node.params:
+        params = [str(p) for p in node.params]
+    else:
+        params = []
+
     if node.opname == 'and':
-        return '( %s )' % (' && '.join(ops)
+        return '( %s )' % (' && '.join(ops))
     elif node.opname == 'or':
-        return '( %s )' % (' || '.join(ops)
+        return '( %s )' % (' || '.join(ops))
     elif node.opname == 'not':
         assert len(ops) == 1
         return '!( %s )' % str(ops[0])
     elif node.opname == 'xor':
-        return '( %s )' % (' ^ '.join(ops)
+        return '( %s )' % (' ^ '.join(ops))
     elif node.opname == 'add':
-        return '( %s )' % (' + '.join(ops)
+        return '( %s )' % (' + '.join(ops))
     elif node.opname == 'sub':
         assert len(ops) == 2
-        return '( %s )' % (' - '.join(ops)
+        return '( %s )' % (' - '.join(ops))
     elif node.opname == 'distinct':
         assert len(ops) == 2
-        return '( %s )' % (' != '.join(ops)
+        return '( %s )' % (' != '.join(ops))
     elif node.opname == 'eq':
         assert len(ops) == 2
-        return '( %s )' % (' == '.join(ops)
+        return '( %s )' % (' == '.join(ops))
     elif node.opname == 'rr':
         assert len(ops) == 1
         w = node.width
@@ -160,15 +217,15 @@ def z3op2verilog(node, ctx):
     elif node.opname == 'slt':
         sops = ['$signed(%s)' % ctx.getExpr(op) for op in node.operands]
         assert len(sops) == 2
-        return '( %s )' % (' < '.join(sops)
+        return '( %s )' % (' < '.join(sops))
     elif node.opname == 'sgt':
         sops = ['$signed(%s)' % ctx.getExpr(op) for op in node.operands]
         assert len(sops) == 2
-        return '( %s )' % (' > '.join(sops)
+        return '( %s )' % (' > '.join(sops))
     elif node.opname == 'ult':
-        return '( %s )' % (' < '.join(ops)
+        return '( %s )' % (' < '.join(ops))
     elif node.opname == 'ugt':
-        return '( %s )' % (' > '.join(ops)
+        return '( %s )' % (' > '.join(ops))
     elif node.opname == 'zero-ext':
         assert len(ops) == 1
         assert len(params) == 1
@@ -181,40 +238,44 @@ def z3op2verilog(node, ctx):
         return '{ %s{%s}, %s }' % (params[0], ops[0], ops[0])
     elif node.opname == 'lshift':
         assert len(ops) == 2
-        return '( %s )' % (' << '.join(ops)
+        return '( %s )' % (' << '.join(ops))
     elif node.opname == 'rshift':
         assert len(ops) == 2
-        return '( %s )' % (' >> '.join(ops)
+        return '( %s )' % (' >> '.join(ops))
     elif node.opname == 'cpl':
         assert len(ops) == 1
         return '~( %s )' % str(ops[0])
     elif node.opname == 'bvand':
         assert len(ops) == 2
-        return '( %s )' % (' & '.join(ops)
+        return '( %s )' % (' & '.join(ops))
     elif node.opname == 'bvor':
         assert len(ops) == 2
-        return '( %s )' % (' | '.join(ops)
+        return '( %s )' % (' | '.join(ops))
     elif node.opname == 'bvxor':
         assert len(ops) == 2
-        return '( %s )' % (' ^ '.join(ops)
+        return '( %s )' % (' ^ '.join(ops))
     elif node.opname == 'bvxnor':
         assert len(ops) == 2
-        return '~( %s )' % (' ^ '.join(ops)
+        return '~( %s )' % (' ^ '.join(ops))
     elif node.opname == 'bvdiv':
         assert len(ops) == 2
-        return '( %s )' % (' / '.join(ops)
+        return '( %s )' % (' / '.join(ops))
     elif node.opname == 'bvrem':
         assert len(ops) == 2
-        return '( %s )' % (' % '.join(ops)
+        return '( %s )' % (' % '.join(ops))
     elif node.opname == 'bvmul':
         assert len(ops) == 2
-        return '( %s )' % (' * '.join(ops)
+        return '( %s )' % (' * '.join(ops))
 
 
 if __name__ == '__main__':
     asts = readAllASTs(sys.argv[1])
+    vctx = VerilogContext()
     assert len(asts) == 0x100
     for astdict in asts:
         assert len(astdict) == 24
+        for k, v in astdict.iteritems():
+            vctx.getExpr(v)
+
 
     print 'all good!'
