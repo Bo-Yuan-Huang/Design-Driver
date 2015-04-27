a27bca0bafcc28bfbe12506682537f5fbc499f50
Pramod Subramanyan <pramod.subramanyan@gmail.com>
2015-04-27 18:19:59 -0400

    changing the cxrom to run the SHA test.

--- secureboot8051/fw8051/uctrl/asm/sha_test.c
+++ secureboot8051/fw8051/uctrl/asm/sha_test.c
@@ -25,8 +25,9 @@ __xdata __at(0xFE01) unsigned char aes_reg_state;
 __xdata __at(0xFE02) unsigned int aes_reg_rd_addr;
 __xdata __at(0xFE04) unsigned int aes_reg_wr_addr;
 __xdata __at(0xFE06) unsigned int aes_reg_len;
-__xdata __at(0xE000) unsigned char data[32];
-__xdata __at(0xE100) unsigned char hash[20];
+__xdata __at(0xE000) unsigned char d1[64];
+__xdata __at(0xE100) unsigned char d2[128];
+__xdata __at(0xE200) unsigned char hash[20];
 
 /*---------------------------------------------------------------------------*/
 
@@ -36,14 +37,17 @@ void main() {
     int good=1;
 
     // test writing to XRAM.
-    for(i=0; i < 32; i++) {
-        data[i]=i;
+    for(i=0; i < 64; i++) {
+        d1[i]=0x41;
+    }
+    for(i=0; i < 128; i++) {
+        d2[i] = i;
     }
 
     // setup address, length, counter and key.
-    aes_reg_rd_addr = (unsigned int) &data;
+    aes_reg_rd_addr = (unsigned int) &d1;
     aes_reg_wr_addr = (unsigned int) &hash;
-    aes_reg_len = 32;
+    aes_reg_len = 64;
 
     // now start encryption.
     aes_reg_start = 1;
@@ -52,9 +56,23 @@ void main() {
 
     // read encrypted data and dump it to P0.
     for(i=0; i < 20; i++) {
-        P1 = i;
+        P1 = 1;
         P0 = hash[i];
     }
+
+    aes_reg_rd_addr = (unsigned int) &d2;
+    aes_reg_len = 128;
+    // now start encryption.
+    aes_reg_start = 1;
+    // now wait for encryption to complete.
+    while(aes_reg_state != 0);
+
+    // read encrypted data and dump it to P0.
+    for(i=0; i < 20; i++) {
+        P1 = 2;
+        P0 = hash[i];
+    }
+
 
     // finish.
     quit();
