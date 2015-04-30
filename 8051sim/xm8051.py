c16e1fc43e26d46deaf82a367eddba6897273d12
Pramod Subramanyan <pramod.subramanyan@gmail.com>
2015-04-30 18:04:23 -0400

    finally the stupid model works.

--- 8051sim/xm8051.py
+++ 8051sim/xm8051.py
@@ -1,5 +1,7 @@
 # A simple simulator for the external memory, AES and SHA modules.
 import sys
+from Crypto.Cipher import AES
+from Crypto.Hash import SHA
 
 class XMM(object):
     STEP = 0
@@ -19,17 +21,27 @@ class XMM(object):
     def __init__(self):
         self.regmap = {}
         self.regidx = {}
+        self.addReg('aes_start', 0xff00, 1, readonly=True)
         self.addReg('aes_state', 0xff01, 1)
         self.addReg('aes_addr', 0xff02, 2)
         self.addReg('aes_len', 0xff04, 2)
         self.addReg('aes_ctr', 0xff10, 0x10)
         self.addReg('aes_key0', 0xff20, 0x10)
         self.addReg('aes_key1', 0xff30, 0x10)
+        self.addReg('sha_start', 0xfe00, 1, readonly=True)
         self.addReg('sha_state', 0xfe01, 1)
         self.addReg('sha_rdaddr', 0xfe02, 2)
         self.addReg('sha_wraddr', 0xfe04, 2)
         self.addReg('sha_len', 0xfe06, 2)
-        self.addReg('sha_reserved', 0xfe08, 2, 1)
+        self.addReg('sha_reserved_2', 0xfe08, 2, readonly=True)
+
+        self.aes_bytes_processed = 0
+        self.aes_read_data = [0] * 16
+        self.aes_enc_data  = [0] * 16
+
+        self.sha_bytes_processed = 0
+        self.sha_read_data = [0] * 64
+        self.sha_digest = [0] * 20
 
         self.xram = [0]*65536
 
@@ -83,17 +95,103 @@ class XMM(object):
 
     def write(self, addr, datain):
         assert addr >= 0 and addr <= 65536
+        assert datain >=0 and datain <= 256
+
         for (a0, a1) in self.regmap:
             if addr >= a0 and addr < a1:
                 (name, length, readonly, data) = self.regmap[(a0, a1)]
+
                 if readonly: 
+                    if addr == 0xff00 and self.getRegI('aes_state') == XMM.AES_IDLE and (datain % 2) == 1:
+                        self.setRegI('aes_state', XMM.AES_RD)
+                        self.aes_bytes_processed = 0
+                    elif addr == 0xfe00 and self.getRegI('sha_state') == XMM.SHA_IDLE and (datain % 2) == 1:
+                        self.setRegI('sha_state', XMM.SHA_RD)
+                        self.sha_bytes_processed = 0
+                        self.sha_digest = [0]*20
                     return
                 else:
+                    # no writes when in operation.
+                    if addr >= 0xff00 and addr < 0xff40 and self.getRegI('aes_state') != XMM.AES_IDLE:
+                        return
+                    if addr >= 0xfe00 and addr < 0xfe10 and self.getRegI('sha_state') != XMM.SHA_IDLE:
+                        return
+
                     offset = addr-a0
                     data[offset] = datain
                     self.regmap[(a0, a1)] = (name, length, readonly, data)
+                    return
         self.xram[addr] = datain
 
+    def aes_step(self):
+        aes_state = self.getRegI('aes_state')
+        aes_addr = self.getRegI('aes_addr')
+        if aes_state == XMM.AES_RD:
+            self.aes_read_data = [self.xram[i+aes_addr+self.aes_bytes_processed] for i in xrange(16)]
+            self.setRegI('aes_state', XMM.AES_OP)
+        elif aes_state == XMM.AES_OP:
+            aes_bytes_in = bytes(''.join([chr(ci) for ci in self.aes_read_data]))
+            aes_ctr = lambda: bytes(''.join([chr(ci) for ci in self.getRegB('aes_ctr')]))
+            aes_key = bytes(''.join([chr(ci) for ci in self.getRegB('aes_key0')]))
+            aes = AES.new(key=aes_key, mode=AES.MODE_CTR, counter=aes_ctr)
+            self.aes_enc_data = [ord(a) for a in aes.encrypt(aes_bytes_in)]
+            self.setRegI('aes_state', XMM.AES_WR)
+
+            assert len(self.aes_read_data) == 16
+            assert len(aes_bytes_in) == 16
+            assert len(aes_ctr()) == 16
+            assert len(aes_key) == 16
+            assert len(self.aes_enc_data) == 16
+
+        elif aes_state == XMM.AES_WR:
+            for i in xrange(16):
+                addr = aes_addr + i + self.aes_bytes_processed
+                self.xram[addr] = self.aes_enc_data[i]
+            self.aes_bytes_processed += 16
+            if self.aes_bytes_processed < self.getRegI('aes_len'):
+                self.setRegI('aes_state', XMM.AES_RD)
+            else:
+                self.setRegI('aes_state', XMM.AES_IDLE)
+        else:
+            assert aes_state == XMM.AES_IDLE
+
+
+    def sha_step(self):
+        sha_state = self.getRegI('sha_state')
+        sha_rdaddr = self.getRegI('sha_rdaddr')
+        sha_wraddr = self.getRegI('sha_wraddr')
+        sha_len = self.getRegI('sha_len')
+
+        if sha_state == XMM.SHA_RD:
+            self.sha_read_data = [self.xram[i+sha_rdaddr+self.sha_bytes_processed] for i in xrange(64)]
+            self.setRegI('sha_state', XMM.SHA_OP)
+        elif sha_state == XMM.SHA_OP:
+            assert len(self.sha_digest) == 20
+
+            sha_bytes_in = bytes(''.join([chr(c) for c in self.sha_digest + self.sha_read_data]))
+            h = SHA.new()
+            h.update(sha_bytes_in)
+            self.sha_digest = [ord(a) for a in h.digest()]
+            self.sha_bytes_processed += 64
+
+            if self.sha_bytes_processed < sha_len:
+                self.setRegI('sha_state', XMM.SHA_RD)
+            else:
+                self.setRegI('sha_state', XMM.SHA_WR)
+
+            assert len(self.sha_read_data) == 64
+            assert len(sha_bytes_in) == (64 + 20)
+            assert len(self.sha_digest) == 20
+        elif sha_state == XMM.SHA_WR:
+            assert len(self.sha_digest) == 20
+            for i in xrange(20):
+                addr = i + sha_wraddr
+                self.xram[addr] = self.sha_digest[i]
+            self.setRegI('sha_state', XMM.SHA_IDLE)
+
+        else:
+            assert sha_state == XMM.SHA_IDLE
+                        
     def operate(self, op, addr, datain):
         if op == XMM.RD:
             return self.read(addr)
@@ -101,8 +199,9 @@ class XMM(object):
             self.write(addr, datain)
             return 0
         else:
-            pass
-
+            assert op == XMM.STEP
+            self.aes_step()
+            self.sha_step()
 
 def test(argv):
     xmm = XMM()
@@ -117,7 +216,73 @@ def test(argv):
         assert xmm.operate(XMM.RD, 0xff10+i, 1) == i
     assert xmm.operate(XMM.RD, 0xfe06, 0) == 0
     assert xmm.operate(XMM.RD, 0xfe07, 0) == 1
+    for i in xrange(16):
+        xmm.operate(XMM.WR, 0xff20+i, i+1)
+    xmm.operate(XMM.WR, 0xff02, 0x00)
+    xmm.operate(XMM.WR, 0xff03, 0x10)
+    xmm.operate(XMM.WR, 0xff04, 0x20)
+    xmm.operate(XMM.WR, 0xff05, 0x00)
 
+    for i in xrange(0x20):
+        xmm.operate(XMM.WR, 0x1000+i, 65+i)
+
+    xmm.operate(XMM.WR, 0xff00, 1)
+    assert xmm.operate(XMM.RD, 0xff01, 1) == XMM.AES_RD
+
+    xmm.operate(XMM.STEP, 0, 0)
+    assert xmm.operate(XMM.RD, 0xff01, 1) == XMM.AES_OP
+
+    xmm.operate(XMM.STEP, 0, 0)
+    assert xmm.operate(XMM.RD, 0xff01, 1) == XMM.AES_WR
+
+    xmm.operate(XMM.STEP, 0, 0)
+    assert xmm.operate(XMM.RD, 0xff01, 1) == XMM.AES_RD
+
+    xmm.operate(XMM.STEP, 0, 0)
+    assert xmm.operate(XMM.RD, 0xff01, 1) == XMM.AES_OP
+
+    xmm.operate(XMM.STEP, 0, 0)
+    assert xmm.operate(XMM.RD, 0xff01, 1) == XMM.AES_WR
+
+    xmm.operate(XMM.STEP, 0, 0)
+    assert xmm.operate(XMM.RD, 0xff01, 1) == XMM.AES_IDLE
+
+    data = []
+    for i in xrange(0x20):
+        data.append(xmm.operate(XMM.RD, 0x1000+i, 65+i))
+    print data
+
+    xmm.operate(XMM.WR, 0xff00, 1)
+    while xmm.operate(XMM.RD, 0xff01, 0) != XMM.AES_IDLE:
+        xmm.operate(XMM.STEP, 0, 0)
+
+    for i in xrange(0x20):
+        assert xmm.operate(XMM.RD, 0x1000+i, 0) == 65+i
+
+    assert xmm.getRegI('sha_state') == XMM.SHA_IDLE
+    xmm.operate(XMM.WR, 0xfe02, 00)
+    xmm.operate(XMM.WR, 0xfe03, 0x10)
+    xmm.operate(XMM.WR, 0xfe04, 00)
+    xmm.operate(XMM.WR, 0xfe05, 0x20)
+    xmm.operate(XMM.WR, 0xfe06, 40)
+    xmm.operate(XMM.WR, 0xfe07, 00)
+
+    assert xmm.getRegI('sha_rdaddr') == 0x1000
+    assert xmm.getRegI('sha_wraddr') == 0x2000
+
+    for i in xrange(0x40):
+        xmm.operate(XMM.WR, 0x1000+i, i)
+
+    xmm.operate(XMM.WR, 0xfe00, 1)
+    assert xmm.operate(XMM.RD, 0xfe01, 0) == XMM.SHA_RD
+
+    while xmm.operate(XMM.RD, 0xfe01, 0) != XMM.SHA_IDLE:
+        xmm.operate(XMM.STEP, 0, 0)
+
+    data = []
+    for i in xrange(20):
+        data.append(xmm.operate(XMM.RD, 0x2000+i, 0))
+    print data, 'yes'
 
 if __name__ == '__main__':
     test(sys.argv)
